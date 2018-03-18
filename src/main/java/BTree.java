d78910608771364d75ca9d80b1cb24ff89f378e1
dmyrdek <dmyrdek@oswego.edu>
2018-03-18 14:39:50 -0400

    Created Node class

--- src/main/java/BTree.java
+++ src/main/java/BTree.java
@@ -1,2 +1,20 @@
 public class BTree {
+    private static class Node{
+        private int key;
+        private int childCount;
+        private Business[] businesses;
+        private Node[] children;
+        private boolean isLeft;
+
+        public Node(boolean l){
+            businesses = new Business[2 * minDegree - 1];
+            children = new Node[2 * minDegree];
+            key = 0;
+            childCount = 0;
+        }
+    }
+
+    private static Node root;
+    private static int minDegree;
+
 }
