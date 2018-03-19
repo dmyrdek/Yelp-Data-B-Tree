ae6f150d028f683c23ff25ac0d79f905c6038b8a
dmyrdek <dmyrdek@oswego.edu>
2018-03-18 23:08:06 -0400

    Updated node class

--- src/main/java/BTree.java
+++ src/main/java/BTree.java
@@ -1,16 +1,59 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class BTree {
     private static class Node{
-        private int key;
+        private int keys;
         private int childCount;
         private Business[] businesses;
         private Node[] children;
-        private boolean isLeft;
+        private boolean isLeaf;
 
         public Node(boolean l){
             businesses = new Business[2 * minDegree - 1];
             children = new Node[2 * minDegree];
-            key = 0;
+            keys = 0;
             childCount = 0;
+        }
+
+        public Node[] getChildren() {
+            return children;
+        }
+
+        public Business[] getBusinesses() {
+            return businesses;
+        }
+
+        public boolean isLeaf(){
+            return isLeaf;
+        }
+
+        public int getChildCount() {
+            return childCount;
+        }
+
+        private void sortBusinesses(){
+            Arrays.sort(getBusinesses(),0,keys);
+        }
+
+        public void insertBusiness(Business b){
+            businesses[keys] = b;
+            keys++;
+            sortBusinesses();
+        }
+
+        public void removeChild(int i){
+            children[i] = null;
+            childCount--;
+        }
+
+        public void removeBusiness(int i){
+            businesses[i] = null;
+            keys--;
+        }
+
+        public int middleKey(){
+            return getBusinesses().length / 2;
         }
     }
 
